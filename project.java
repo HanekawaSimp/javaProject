c5b7adbe0d5d3ea59e0ca3b7a35c861473f46ccf
HanekawaSimp <98197509+HanekawaSimp@users.noreply.github.com>
2023-04-21 07:15:42 +0530

    Update project.java

--- project.java
+++ project.java
@@ -1,28 +1,32 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-class project {
+class Project {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<EmergencyCall> fireCalls = new ArrayList<>();
-        ArrayList<EmergencyCall> policeCalls = new ArrayList<>();
-        ArrayList<EmergencyCall> ambulanceCalls = new ArrayList<>();
-        
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<EmergencyCall> fireCalls = new TreeSet<>();
+        TreeSet<EmergencyCall> policeCalls = new TreeSet<>();
+        TreeSet<EmergencyCall> ambulanceCalls = new TreeSet<>();
+
         while (true) {
-            System.out.println("Enter 1 to record a call, 2 to remove a call, 3 to generate reports, or 4 to quit:");
-            int choice = sc.nextInt();
-            
-            if (choice == 1) {
+            System.out.println("Emergency Call System");
+            System.out.println("Please choose an option:");
+            System.out.println("a. Enter 1 to record a call");
+            System.out.println("b. Enter 2 to remove a call");
+            System.out.println("c. Enter 3 to generate a report");
+            System.out.println("d. Enter 4 to quit");
+
+            String choice = scanner.next();
+
+            if (choice.equals("1")) {
                 EmergencyCall call = new EmergencyCall();
                 System.out.println("Enter the caller's name:");
-                sc.nextLine();
-                call.setCallerName(sc.nextLine());
+                scanner.nextLine();
+                call.setCallerName(scanner.nextLine());
                 System.out.println("Enter a brief description of the emergency:");
-                call.setDescription(sc.nextLine());
+                call.setDescription(scanner.nextLine());
                 System.out.println("Enter 1 to request fire service, 2 to request police service, 3 to request ambulance service, or any combination of these to request multiple services:");
-                int serviceType = sc.nextInt();
+                int serviceType = scanner.nextInt();
                 if (serviceType % 10 == 1) {
                     fireCalls.add(call);
                 }
@@ -32,39 +36,38 @@ class project {
                 if (serviceType % 10 == 3) {
                     ambulanceCalls.add(call);
                 }
-            } else if (choice == 2) {
+            } else if (choice.equals("2")) {
                 System.out.println("Enter 1 to remove a fire call, 2 to remove a police call, 3 to remove an ambulance call:");
-                int serviceType = sc.nextInt();
+                int serviceType = scanner.nextInt();
                 System.out.println("Enter the index of the call to remove:");
-                int index = sc.nextInt() - 1;
+                int index = scanner.nextInt() - 1;
                 if (serviceType == 1) {
-                    fireCalls.remove(index);
+                    fireCalls.remove(fireCalls.toArray()[index]);
                 } else if (serviceType == 2) {
-                    policeCalls.remove(index);
+                    policeCalls.remove(policeCalls.toArray()[index]);
                 } else if (serviceType == 3) {
-                    ambulanceCalls.remove(index);
+                    ambulanceCalls.remove(ambulanceCalls.toArray()[index]);
                 }
-            } else if (choice == 3) {
+            } else if (choice.equals("3")) {
                 System.out.println("Fire calls:");
                 printCalls(fireCalls);
                 System.out.println("Police calls:");
                 printCalls(policeCalls);
                 System.out.println("Ambulance calls:");
                 printCalls(ambulanceCalls);
-            } else if (choice == 4) {
+            } else if (choice.equals("4")) {
                 break;
             }
         }
     }
-    
-    static void printCalls(ArrayList<EmergencyCall> calls) {
-        Collections.sort(calls);
-        for (int i = 0; i < calls.size(); i++) {
-            EmergencyCall call = calls.get(i);
-            System.out.println((i + 1) + ". " + call.getCallerName() + ": " + call.getDescription());
+
+    static void printCalls(TreeSet<EmergencyCall> calls) {
+        int index = 1;
+        for (EmergencyCall call : calls) {
+            System.out.println(index + ". " + call.getCallerName() + ": " + call.getDescription());
+            index++;
         }
     }
-
 }
 
 class EmergencyCall implements Comparable<EmergencyCall> {
